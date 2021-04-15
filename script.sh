7775023b4967e0ced4e2cd7ae0852e7282fcd1fb
ZainulNiaz <zainul_n@cs.iitr.ac.in>
2021-04-16 04:53:59 +0530

    script

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+output=$(pwd)
+FILE="/home/zainul/.gitconfig"
+string=$(grep -w "signingkey"  "${FILE}")
+echo "$string"
+if [[ $string == *"signingkey"* ]];
+then 
+		echo "GPGKey already exists"
+else
+		echo "GPGkey doesnt exist... Creating a new key"
+		gpg --gen-key
+		z=$(gpg --list-keys | grep -A1 "pub   rsa" )
+		x=${z:58}
+		git config --global user.signingkey $x
+		
+fi
+
+
+z=$(gpg --list-keys | grep -A1 "pub   rsa" )
+x=${z:58}
+echo "${x}"
+git config --global commit.gpgsign true
+gpg --armor --export $x
+echo "Copy and paste this in github/settings/ssh and gpg keys/gpg keys"
+echo "completed"
+
+
+
+
+
+
+
+# sed -i 's/\r$//'	script.sh	
